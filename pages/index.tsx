6418f48a01e142074a76bd591796e50514d57850
Rohit <rohitp934@gmail.com>
2022-06-29 09:45:06 +0530

    Show website under development in main

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,14 @@ import type { NextPage } from 'next';
 const Home: NextPage = () => {
 	return (
 		<>
-			<h1 className="font-bold">
-				Hello <span className="text-teal-400">Next.js</span>
-			</h1>
+			<div className="flex flex-col justify-center items-center h-[100vh]">
+				<h1 className="text-[5rem] text-slate inline-block">
+					Website currently being <span className="text-green">developed</span>
+					<h2 className="text-[2rem] text-slate text-right block pt-6">
+						- Built with Next.Js and TypeScript ❤️
+					</h2>
+				</h1>
+			</div>
 		</>
 	);
 };
