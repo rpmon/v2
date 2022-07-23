fa993e7671ecca0df6d89172c01c1851070d5d49
Rohit <rohitp934@gmail.com>
2022-07-23 14:24:24 +0530

    Fix loader positioning

--- components/loader.tsx
+++ components/loader.tsx
@@ -17,7 +17,7 @@ const Loader = ({ finishLoading }: loaderProps) => {
 	return (
 		<div className="m-0 font-mono min-h-screen grid place-content-center text-center dark:bg-black">
 			<h1
-				className="text-typography relative w-max before:absolute before:inset-0 after:absolute after:inset-0
+				className="text-typography relative w-max sm:w-full before:absolute before:inset-0 after:absolute after:inset-0
 			before:bg-white dark:before:bg-black before:animate-loader-before
 			after:w-[0.125em] after:bg-slate dark:after:bg-white after:animate-loader-after
 			"
@@ -26,7 +26,7 @@ const Loader = ({ finishLoading }: loaderProps) => {
 			</h1>
 			<h2
 				id="subtitle"
-				className="text-heading font-mono text-sky dark:text-aqua font-semibold pt-4 opacity-0 transition-opacity duration-500"
+				className="text-typography font-mono text-sky dark:text-aqua font-semibold pt-4 opacity-0 transition-opacity duration-500"
 			>
 				This is my Developer Portfolio.
 			</h2>
